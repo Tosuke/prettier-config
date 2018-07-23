e69ca064333d0679c1389acf73beb81fe08bab5e
Tosuke <tasukeprg@gmail.com>
2018-07-23 12:37:38 +0900

    :rocket:

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+}
